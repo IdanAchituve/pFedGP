7a02e183a205474889a933be0204e0fbe4e50247
root <root@com120226.localdomain>
2021-06-02 14:47:08 +0300

    bug fixes

--- experiments/datafolder/noisy_cifar10/download_noisy_data.py
+++ experiments/datafolder/noisy_cifar10/download_noisy_data.py
@@ -1,5 +1,5 @@
 import gdown
 
-url = 'https://drive.google.com/uc?id=1tz_lMUHojjXvdHiVaaBEw1fwItY1BrEc'
+url = 'https://drive.google.com/uc?id=1clA-tCsfuCIbOTMHrHOHhj_TVJVzkDcy'
 output = 'PointSegDAdataset.rar'
 gdown.download(url, output, quiet=False)
--- experiments/datafolder/noisy_cifar100/download_noisy_data.py
+++ experiments/datafolder/noisy_cifar100/download_noisy_data.py
@@ -1,5 +1,5 @@
 import gdown
 
-url = 'https://drive.google.com/uc?id=1tz_lMUHojjXvdHiVaaBEw1fwItY1BrEc'
+url = 'https://drive.google.com/uc?id=1FYgoksNhDr_2WwQ7L0nVgZ_d9MRpvTh6'
 output = 'PointSegDAdataset.rar'
 gdown.download(url, output, quiet=False)
