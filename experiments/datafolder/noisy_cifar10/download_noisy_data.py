19b912a33b5ba7b8a7328d4a76b161ae84b73cbb
root <root@com120226.localdomain>
2021-06-02 14:51:07 +0300

    bug fixes

--- experiments/datafolder/noisy_cifar10/download_noisy_data.py
+++ experiments/datafolder/noisy_cifar10/download_noisy_data.py
@@ -1,5 +1,5 @@
 import gdown
 
 url = 'https://drive.google.com/uc?id=1tz_lMUHojjXvdHiVaaBEw1fwItY1BrEc'
-output = 'PointSegDAdataset.rar'
+output = 'data_dictionary.pkl'
 gdown.download(url, output, quiet=False)
--- experiments/datafolder/noisy_cifar100/download_noisy_data.py
+++ experiments/datafolder/noisy_cifar100/download_noisy_data.py
@@ -1,5 +1,5 @@
 import gdown
 
 url = 'https://drive.google.com/uc?id=1V-Dwfl6RXVpmKB2lZFIehhJcE5_rYs8i'
-output = 'PointSegDAdataset.rar'
+output = 'data_dictionary.pkl'
 gdown.download(url, output, quiet=False)
